2a20bb2a4adb3c36b31749385ff5847973c6d642
Md Shanoar Showrov <shanoarshowrov@gmail.com>
2025-11-28 17:26:12 +0600

    Add files via upload

---
+++ pattern 1.c
@@ -0,0 +1,17 @@
+ls
+int main ()
+{
+ int i,j;
+ for(i=0;i<=5;i++) //this loop difine rows
+ {
+     for(j=0;j<=i;j++) // this loop difine colums
+     {
+        printf("Showrov\t");
+     }
+     printf("\n");
+ }
+
+
+
+   return 0;
+    }
