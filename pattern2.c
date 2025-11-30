64d5e7eba37adb68f5ca2d963c6a0f3143558408
Md Shanoar Showrov <shanoarshowrov@gmail.com>
2025-11-30 19:56:57 +0600

    Create pattern2.c
    2nd pattern

---
+++ pattern2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+ int i,j;
+ for(i=5;i>0;i--) //this loop difine rows
+ {
+     for(j=1;j<=i;j++) // this loop difine colums
+     {
+        printf("Showrov\t");
+     }
+     printf("\n");
+ }
+
+
+
+   return 0;
+    }
